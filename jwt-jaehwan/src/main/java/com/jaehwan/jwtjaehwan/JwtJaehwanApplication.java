b33176366837c039428dce09491eb327b4bd8d0c
gh03014 <jaehwan3399@gmail.com>
2021-03-28 21:49:28 +0900

    jwt 3. JWT 설정

---
+++ jwt-jaehwan/src/main/java/com/jaehwan/jwtjaehwan/JwtJaehwanApplication.java
@@ -0,0 +1,13 @@
+package com.jaehwan.jwtjaehwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtJaehwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtJaehwanApplication.class, args);
+	}
+
+}
---
+++ jwt-jaehwan/src/test/java/com/jaehwan/jwtjaehwan/JwtJaehwanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaehwan.jwtjaehwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtJaehwanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
